50ca4ed0f7d36f0de5fcb9b206e7a62983835847
Bobby <103152798+tiiagodcf@users.noreply.github.com>
2023-08-02 14:53:35 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'A script to implement an injured animation and disable running when health is low'
+author 'Your Name'
+version '1.0.0'
+
+client_script 'client.lua'
